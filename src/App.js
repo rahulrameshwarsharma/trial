7269de6e30f4051a89ae6e06eedbd40f52dae1d2
rahulrameshwarsharma <91724758+rahulrameshwarsharma@users.noreply.github.com>
2022-12-22 22:26:07 +0530

    major changes in codes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 function App() {
   return (
     <div className="app">
-      <h1 className="nav-title">Hello Rahul project achche se banana!!!</h1>
+      {/* <h1 className="nav-title">Hello Rahul project achche se banana!!!</h1> */}
+      <h1 className="nav-title">Under Development!!!</h1>
     </div>
   );
 }
