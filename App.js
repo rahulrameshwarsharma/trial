8b603030422760bc6b5fa76d0b4441fbd7ed8ca4
rahulrameshwarsharma <91724758+rahulrameshwarsharma@users.noreply.github.com>
2022-12-22 22:11:20 +0530

    changed some words

---
+++ App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h1 className="nav-title">Hello Rahul project achche se banana!!!</h1> */}
+      <h1 className="nav-title">Under Development!!!</h1>
+    </div>
+  );
+}
+
+export default App;
